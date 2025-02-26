c0d80b6d8f829b070daca5a0a9a10b6692edceb6
Eric Saldivar <esaldivar1214@gmail.com>
2025-02-26 13:33:20 -0600

    Add header and dashboard components with initial templates and styles

---
+++ src/app/dashboard/server-traffic/server-traffic.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server-traffic',
+  standalone: true,
+  imports: [],
+  templateUrl: './server-traffic.component.html',
+  styleUrl: './server-traffic.component.css'
+})
+export class ServerTrafficComponent {
+
+}
